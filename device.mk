5824487d2a54333a057744cef13c1adcc4d370b3
drpradsms <dr.pradsms@gmail.com>
2019-10-04 10:47:03 +0530

    Revert "replace msm_irqbalance.conf"
    This reverts commit 36ef0031c0a188969650b793a7ea71126c9410ad.
    
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- device.mk
+++ device.mk
@@ -73,8 +73,7 @@ PRODUCT_PACKAGES += \
     RemovePackages
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
 # Boot control
 PRODUCT_PACKAGES_DEBUG += \
