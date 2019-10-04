36d0b2d8f736194747f295b48f938c454b606cc8
drpradsms <dr.pradsms@gmail.com>
2019-10-04 09:51:39 +0530

    Revert "BoardConfig: Properly set dexpreopt"
    This reverts commit 7bc0cbf228fda65a702e7b1e65bc5d14fcc37ebd.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,17 +104,13 @@ TARGET_USES_MEDIA_EXTENSIONS := false
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# Enable dexpreopt to speed boot time
+# Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_DEBUG_INFO := false
-      USE_DEX2OAT_DEBUG := false
-      DONT_DEXPREOPT_PREBUILTS := true
-      WITH_DEXPREOPT_PIC := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    WITH_DEXPREOPT ?= true
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 TARGET_USES_HWC2 := true
