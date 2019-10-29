350b2a6c3f496ad3c05e77957038c365c37498cb
drpradsms <dr.pradsms@gmail.com>
2019-10-29 12:18:23 +0530

    Welcome enforcing
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(VENDOR_PATH)/bluetooth/include
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -180,7 +179,7 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/public \
     device/qcom/sepolicy/qva/public
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicytemp
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicytemp
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(VENDOR_PATH)
